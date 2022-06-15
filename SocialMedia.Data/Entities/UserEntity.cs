ca6c9dd510c865d098adc842227cf9bda34c0d49
hayzemke <hay.zemke@gmail.com>
2022-06-15 19:15:26 -0400

    finaleeee

--- SocialMedia.Data/Entities/UserEntity.cs
+++ SocialMedia.Data/Entities/UserEntity.cs
@@ -1,10 +1,14 @@
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
 
 public class UserEntity
     {   [Key]
         public int ID { get; set; }
 
-        [Required]
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
@@ -16,9 +20,6 @@ public class UserEntity
 
         [Required]
         public string Password { get; set; }
-
-        [Required]
-        public string ConfirmPassword { get; set; }
 
         public DateTime DateJoined { get; set; }
     }
