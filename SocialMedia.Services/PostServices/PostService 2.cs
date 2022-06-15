01fbbbbe07d88f6532eed07141b7e28a4187065b
hayzemke <hay.zemke@gmail.com>
2022-06-14 20:32:48 -0400

    FINAL

---
+++ SocialMedia.Services/PostServices/PostService 2.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+public class PostService : IPostService
+    {
+        private readonly SocialMedia_DbContext _context;
+        private readonly int _postID;
+        public PostService(SocialMedia_DbContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> RegisterPostAsync(PostCreate model)
+        {
+            var entity = new Post
+            {
+                Title = model.Title,
+                Text = model.Text,
+            };
+            
+            _context.Posts.Add(entity);
+            var numberOfChanges = await _context.SaveChangesAsync();
+            return numberOfChanges == 1;
+        }
+
+        public async Task<IEnumerable<PostListItem>> GetPostListItemsAsync()
+        {
+            var posts = await _context.Posts
+            .Where(entity => entity.ID == _postID)
+            .Select(entity => new PostListItem
+            {
+                Id = entity.ID,
+                Title = entity.Title,
+            })
+            .ToListAsync();
+        return posts;
+        }
+    }
